c4bcc6fec73385b8d7d5669831a860f223e04e35
Robin Von <robinholdingon@gmail.com>
2013-06-30 13:56:07 -0800

    Added PropertyLoader

--- Funjoid/src/com/example/funjoid/MainActivity.java
+++ Funjoid/src/com/example/funjoid/MainActivity.java
@@ -10,9 +10,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Let's start");
+
     }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -21,3 +21,6 @@ public class MainActivity extends Activity {
     }
     
 }
+
+
+
